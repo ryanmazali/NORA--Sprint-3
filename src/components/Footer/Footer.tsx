637c610e40eb36f0ac811061439a9eb8c4e524d8
Ryan Mazali <ryanmazali17@gmail.com>
2026-03-25 01:15:28 -0300

    feat: criação do compenente Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,15 @@
-export const Footer = () => <footer>Footer</footer>
+export const Footer = () => {
+    return (
+        <footer
+            className="
+                bg-[rgb(186,217,244)] text-black text-center
+                mt-8 flex-shrink-0
+                px-4 py-4
+                tablet:px-8 tablet:py-4
+                mobile:text-[0.9rem] mobile:py-[0.8rem]
+            "
+        >
+            <p>&copy; 2025 NORA | Projeto acadêmico FIAP</p>
+        </footer>
+    );
+};
