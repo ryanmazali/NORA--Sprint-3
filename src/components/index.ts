208f09751fa5ecc95ebd32272803cad74dc45497
Ryan Mazali <ryanmazali17@gmail.com>
2026-05-08 18:35:20 -0300

    fix: adjusting the export in the index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,4 @@ export * from "./TeamCard/TeamCard";
 export * from "./BackButton/BackButton";
 export { default as PlataformaLayout } from "./Plataforma/PlataformaLayout/PlataformaLayout";
 export { default as Sidebar } from "./Plataforma/Sidebar/Sidebar";
-export { default as TopBar } from "./Plataforma/Topbar/Topbar";
+export { default as TopBar } from "./Plataforma/Topbar/Topbar"; 
